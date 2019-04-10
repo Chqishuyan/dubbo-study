1d4e7422685a10e0c976e46c80f315825992d2d7
shuyan <18205601657@163.com>
2019-04-10 14:35:25 +0800

    修改dubbo配置

--- order-service-consumer/src/main/java/com/auguigu/consumer/ConsumerStart.java
+++ order-service-consumer/src/main/java/com/auguigu/consumer/ConsumerStart.java
@@ -10,12 +10,13 @@ import java.util.List;
 /**
  * Created by shuyan.qi on 2019/4/10.
  */
-public class Main {
+public class ConsumerStart {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
         classPathXmlApplicationContext.start();
         UserService userService = (UserService) classPathXmlApplicationContext.getBean("userService");
         List<User> users = userService.users();
+        System.out.println(users.get(0));
         //users.stream().forEach((e)-> System.out.println(e));
         System.in.read(); // 按任意键退出
     }
--- order-service-provider/src/main/java/com/atguigu/provider/ProviderStart.java
+++ order-service-provider/src/main/java/com/atguigu/provider/ProviderStart.java
@@ -7,11 +7,11 @@ import java.io.IOException;
 /**
  * Created by shuyan.qi on 2019/4/10.
  */
-public class Main {
+public class ProviderStart {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("dubbo-provider.xml");
         classPathXmlApplicationContext.start();
-        System.out.println("provider Main start...");
+        System.out.println("provider ProviderStart start...");
         System.in.read(); // 按任意键退出
     }
 }
