3ad2e1492fa7987fd871b991ab256ca55d987d73
shuyan <18205601657@163.com>
2019-04-11 13:28:46 +0800

    测试dubbo的多版本功能

--- order-service-provider/src/main/java/com/atguigu/provider/UserServiceImpl01.java
+++ order-service-provider/src/main/java/com/atguigu/provider/UserServiceImpl01.java
@@ -9,10 +9,11 @@ import java.util.List;
 /**
  * Created by shuyan.qi on 2019/4/9.
  */
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl01 implements UserService {
 
     @Override
     public List<User> users() {
+        System.out.println("UserServiceImpl01...old");
         List<User> users = new ArrayList<User>(2);
         users.add(new User("qishuyan    ",0,18,"18349563345","安徽"));
         users.add(new User("wangbdan    ",1,16,"18349563353","安徽"));
---
+++ order-service-provider/src/main/java/com/atguigu/provider/UserServiceImpl02.java
@@ -0,0 +1,27 @@
+package com.atguigu.provider;
+
+import com.atguigu.api.UserService;
+import com.atguigu.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shuyan.qi on 2019/4/9.
+ */
+public class UserServiceImpl02 implements UserService {
+
+    @Override
+    public List<User> users() {
+        System.out.println("UserServiceImpl02...new");
+        List<User> users = new ArrayList<User>(2);
+        users.add(new User("qishuyan    ",0,18,"18349563345","安徽"));
+        users.add(new User("wangbdan    ",1,16,"18349563353","安徽"));
+        try {
+            Thread.sleep(3000);//测试dubbo超时配置
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+}
